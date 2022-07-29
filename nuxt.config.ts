7d9894bde24982d56eb04656d3980e4c99e1fe91
classyrazy <classyrazy014@gmail.com>
2022-07-29 14:28:24 +0100

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+import { defineNuxtConfig } from 'nuxt'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+    modules: ["@nuxt/content", "@nuxtjs/tailwindcss"],
+    content: {
+        markdown: {
+            // prism: {
+            //     theme: "prism-themes/themes/prism-material-dark.css"
+
+            // },
+            toc: {
+                depth: 3,
+                searchDepth: 3,
+            }
+        },
+        highlight: {
+            theme: "dracula-soft"
+        }
+
+    }
+})
